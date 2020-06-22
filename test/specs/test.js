c660066d475b04c89c0c5310289789bc8956b3e2
Arjan Blok <arjan.blok@gmail.com>
2020-06-22 22:51:37 +0200

    Add selenium standalone

--- test/specs/test.js
+++ test/specs/test.js
@@ -4,9 +4,6 @@ describe("Test", () => {
     it("isDisplayed", () => {
         browser.setWindowSize(500, 700);
         browser.url("https://demo.applitools.com/gridHackathonV1.html");
-        browser.saveScreenshot("test.png");
-        const isDisplayed = $("#DIV__mainmenu__15").isDisplayed();
-        console.log(isDisplayed);
-        assert.isFalse(isDisplayed);
+        assert.isFalse($("#DIV__mainmenu__15").isDisplayed());
     });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -45,18 +45,21 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
+    outputDir: "logs",
     capabilities: [{
 
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        // maxInstances: 5,
         //
         browserName: 'chrome',
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
+    }, {
+        browserName: 'firefox'
     }],
     //
     // ===================
@@ -105,7 +108,23 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: [
+        ['selenium-standalone', {
+            logPath: 'logs',
+            installArgs: {
+                drivers: {
+                    chrome: { version: '83.0.4103.39' },
+                    firefox: { version: '0.26.0' }
+                }
+            },
+            args: {
+                drivers: {
+                    chrome: { version: '83.0.4103.39' },
+                    firefox: { version: '0.26.0' }
+                }
+            },
+        }]
+    ],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
